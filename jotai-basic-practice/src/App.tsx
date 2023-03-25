a2b2c782f81c1b35f3aaee8217eb133b3291565a
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-03-25 16:16:46 -0500

    add basic counter

--- jotai-basic-practice/src/App.tsx
+++ jotai-basic-practice/src/App.tsx
@@ -1,5 +1,32 @@
-function App() {
-  return <div></div>;
-}
+import { useState } from 'react';
+
+const Counter1 = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
+
+const Counter2 = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <Counter1 />
+      <Counter2 />
+    </div>
+  );
+};
 
 export default App;
