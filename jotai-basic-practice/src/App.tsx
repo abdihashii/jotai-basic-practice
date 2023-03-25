52f1202cad2624bc52a18e46e503104b233e2609
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-03-25 16:20:34 -0500

    Using jotai and finishing part 1 of the tutorial

--- jotai-basic-practice/src/App.tsx
+++ jotai-basic-practice/src/App.tsx
@@ -1,7 +1,14 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import { atom, useAtom } from 'jotai';
+
+const countAtom = atom(0); // default value is 0
+const countAtom2 = atom(0);
 
 const Counter1 = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useAtom(countAtom); // state only for Counter1
+  // const [count, setCount] = useAtom(countAtom); // state shared between Counter1 and Counter2
+  // const [count, setCount] = useState(0); // state only for this component
+
   return (
     <div>
       <p>Count: {count}</p>
@@ -11,7 +18,10 @@ const Counter1 = () => {
 };
 
 const Counter2 = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useAtom(countAtom2); // state only for Counter2
+  // const [count, setCount] = useAtom(countAtom); // state shared between Counter1 and Counter2
+  // const [count, setCount] = useState(0); // state only for this component
+
   return (
     <div>
       <p>Count: {count}</p>
